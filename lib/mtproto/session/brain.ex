7a2eb548bebed2bc91950eaad66f4aef59168f7f
Timothée Floure <timothee.floure@fnux.ch>
2017-03-15 14:57:44 +0100

    Change notify message to `:recv`

--- lib/mtproto/session/brain.ex
+++ lib/mtproto/session/brain.ex
@@ -52,7 +52,7 @@ defmodule MTProto.Session.Brain do
 
     # Notify the client
     if session.client != nil do
-      send session.client, {:telegram_incoming, session_id, msg}
+      send session.client, {:recv, session_id, msg}
     else
       IO.puts "No client for #{session_id}, printing to console."
       IO.inspect {session_id, msg}

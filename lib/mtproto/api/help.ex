6ab3755a9ef151d4f1bb52e1011e5c327e4c3a3a
Timothée Floure <timothee.floure@fnux.ch>
2017-05-01 16:45:21 +0200

    Populate the MTProto.API.Help module

--- lib/mtproto/api/help.ex
+++ lib/mtproto/api/help.ex
@@ -1,19 +1,39 @@
 defmodule MTProto.API.Help do
   @moduledoc """
-  @TODO
+  As in [core.telegram.org/methods#miscellaneous](https://core.telegram.org/methods#miscellaneous).
   """
 
   @doc """
-    Returns info on data centre nearest to the user.
+  Returns info on data centre nearest to the user.
   """
   def get_config do
     TL.build("help.getConfig", %{})
   end
 
   @doc """
-    Returns current configuration, icluding data center configuration.
+  Returns support id for the ‘ask a question’ feature.
+
+  * `phone_number` - phone number in international format (string)
+  * `user` - TL user object, as explained
+  [here](https://core.telegram.org/type/User).
+  """
+  def get_support(phone, user) do
+    TL.build("help.support", %{phone_number: phone, user: user})
+  end
+
+  @doc """
+  Returns current configuration, including data center configuration.
   """
   def get_nearest_dc do
     TL.build("help.getNearestDc",%{})
+  end
+
+  @doc """
+  Returns the text of an invitation text message.
+
+  * `lang_code` - language code, `ISO 639-1`
+  """
+  def get_invite_text(lang_code) do
+    TL.build("help.getInviteText", %{lang_code: lang_code})
   end
 end
